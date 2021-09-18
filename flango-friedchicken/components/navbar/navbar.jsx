93b1866608324c75719a9c52deb92418967e9023
itofelipe <felipeito96@gmail.com>
2021-09-19 04:06:04 +0900

    Mobile Navbar debug

--- flango-friedchicken/components/navbar/navbar.jsx
+++ flango-friedchicken/components/navbar/navbar.jsx
@@ -7,16 +7,17 @@ import Flango_icon from '../../assets/img/chicken.png'
 
 export default function Header() {
     return (
-        <Navbar fixed="top">
+        <Navbar fixed="top" expand="md">
             <Container>
                 <Navbar.Brand href="#">
                     <Image src={Flango_icon} alt="Flango icon" className="header-icon" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="header_menu">
+                <Navbar.Collapse id="basic-navbar-nav" className="header_menu">
                     <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#home">HOME</Nav.Link>
+                        <Nav.Link href="#link">CHICKEN</Nav.Link>
+                        <Nav.Link href="#link">ORDER</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
