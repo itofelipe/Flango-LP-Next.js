35395ad45eef7820d7d4bce8dd3dd704319113cc
itofelipe <felipeito96@gmail.com>
2021-09-19 21:40:44 +0900

    Mobile Navbar debug

--- flango-friedchicken/pages/index.jsx
+++ flango-friedchicken/pages/index.jsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Header from '../components/navbar/navbar'
 import Home from '../components/template-parts/home'
 
